4eee6ef9b0b35cff8284b80147b4b1aceefa5159
Yliya Pehota <pehotaig@gmail.com>
2020-10-19 22:49:35 +0300

    fix: optimized code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-
   const rules = bracketsConfig.join('').replace(/[\s.,%]/g, '');
 
   let brackets = rules;
   let stack = [];
-  
+  let duplicate = Array.from(brackets).filter((item, index) => Array.from(brackets).indexOf(item) != index);
+
   for(let bracket of str) {
+    
     let bracketsIndex = brackets.indexOf(bracket);
 
     // exception for identical elements | or 7 or 8
-    if (stack.includes(bracketsIndex + 1) && (bracket === "|" || bracket === "7" || bracket === "8")) { 
+    if (stack.includes(bracketsIndex + 1) && duplicate.includes(bracket)) { 
       bracketsIndex = bracketsIndex + 1;
     }
 
