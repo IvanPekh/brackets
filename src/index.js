d98234c045fcd75ee6d5224a6d88807c53399373
Yliya Pehota <pehotaig@gmail.com>
2020-10-19 15:33:06 +0300

    init: task Brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  const rules = bracketsConfig.join('').replace(/[\s.,%]/g, '');
+
+  let brackets = rules;
+  let stack = [];
+  
+  for(let bracket of str) {
+    let bracketsIndex = brackets.indexOf(bracket);
+
+    // exception for identical elements | or 7 or 8
+    if (stack.includes(bracketsIndex + 1) && (bracket === "|" || bracket === "7" || bracket === "8")) { 
+      bracketsIndex = bracketsIndex + 1;
+    }
+
+    if(bracketsIndex % 2 === 0) {
+      stack.push(bracketsIndex + 1);
+      
+    } else {
+      
+      if(stack.pop() !== bracketsIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+  }
